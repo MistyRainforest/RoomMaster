44d8041e7d866e078c10d53425b4f6e06064eaae
Adam <adamtan015@gmail.com>
2019-05-30 14:28:52 +0800

    react-router for multipage app

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,26 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
+import ReactDOM from 'react-dom';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('root')
+);
 
-export default App;
+const element = <h1>Hello, world!</h1>;
+export default element;
