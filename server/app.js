ba32f9b1daa16478ad8e1658ebfed5432f9e6148
Adam <adamtan015@gmail.com>
2019-06-09 00:56:08 +0800

    Linked to firebase DB

--- server/app.js
+++ server/app.js
@@ -1,10 +1,27 @@
+//imports
 var express = require("express");
 var bodyParser = require("body-parser");
 var routes = require("./routes/routes.js");
+var firebase = require("firebase");
+
+//defining express app
 var app = express();
 
+//adding whats needed by express
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
+//init for firebase database
+const firebaseConfig = {
+    apiKey: "AIzaSyAmBDkJ4cUdTfg4A60hyEM654FiRyQlnfc",
+    authDomain: "orbitalroommaster.firebaseapp.com",
+    databaseURL: "https://orbitalroommaster.firebaseio.com",
+    projectId: "orbitalroommaster",
+    storageBucket: "orbitalroommaster.appspot.com",
+    messagingSenderId: "937121360205",
+    appId: "1:937121360205:web:a6a5379761942218"
+  };
+  const firebasedb = firebase.initializeApp(firebaseConfig);
 
 routes(app);
 
