51a0ae074d55aee4dc805ad676e682aa24f8d775
Adam <adamtan015@gmail.com>
2019-06-09 22:46:06 +0800

    Added one basic dummy API call

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var routes = require("./routes/routes.js");
-var firebase = require("firebase");
+var firebaseDB = require("./routes/fire.js");
 
 //defining express app
 var app = express();
@@ -11,21 +11,10 @@ var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-//init for firebase database
-const firebaseConfig = {
-    apiKey: "AIzaSyAmBDkJ4cUdTfg4A60hyEM654FiRyQlnfc",
-    authDomain: "orbitalroommaster.firebaseapp.com",
-    databaseURL: "https://orbitalroommaster.firebaseio.com",
-    projectId: "orbitalroommaster",
-    storageBucket: "orbitalroommaster.appspot.com",
-    messagingSenderId: "937121360205",
-    appId: "1:937121360205:web:a6a5379761942218"
-  };
-  const firebasedb = firebase.initializeApp(firebaseConfig);
-
 routes(app);
 
-const PORT = 3000;
+
+const PORT = 3001;
 
 var server = app.listen(PORT, function() {
     console.log("app running on port.", server.address().port);
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -2,6 +2,15 @@ var appRouter = function (app) {
     app.get("/", function(request, response) {
       response.status(200).send("Welcome to RoomMaster REST API");
     });
+// What to send when API fetch uses 'POST' for url = /user
+    app.post("/user", function(request, response) {
+      console.log(request.body)
+      return response.json({
+        uid: request.body.uid,
+        name: 'john',
+        eat: 'me'
+      })
+    });
   }
 
   
